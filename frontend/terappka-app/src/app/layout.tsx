8b2de5b92490b531abea10de88bcb4e9e2f52355
szywat <szywat04@gmail.com>
2026-01-14 20:22:49 +0100

    Corrected 'inter'

--- frontend/terappka-app/src/app/layout.tsx
+++ frontend/terappka-app/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
 import SessionProviderWrapper from "../components/SessionProviderWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +17,7 @@ export default function RootLayout({
  }) {
   return (
     <html lang="pl">
-      <body className="{inter.className">
+      <body className={inter.className}>
         <SessionProviderWrapper>
           {children}
         </SessionProviderWrapper>
