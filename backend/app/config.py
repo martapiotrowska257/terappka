c72915450c654df719e869ddd6c8351738949a72
=michal_watroba <=m.watroba.046@studms.ug.edu.pl>
2026-01-17 21:10:27 +0100

    adding invisibility env

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,5 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://terappka:admin1@localhost:3306/terappka_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = 'your_jwt_secret_key_here'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
