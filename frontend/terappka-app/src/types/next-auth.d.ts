78e0aa1c58c0555ae04a3da5ad6e7c1af6db2e31
szywat <szywat04@gmail.com>
2026-02-17 21:20:18 +0100

    Expanded next-auth to work with CredentialsProvider

--- frontend/terappka-app/src/types/next-auth.d.ts
+++ frontend/terappka-app/src/types/next-auth.d.ts
@@ -1,11 +1,28 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
     // dodajemy pole 'id' do użytkownika
     // można dodać więcej, jeśli potrzeba
     interface Session {
+        accessToken?: string;
+        error?: string;
         user: {
             id?: string | null | undefined
         } & DefaultSession["user"]
+    }
+
+    interface User extends DefaultUser {
+        accessToken?: string;
+        refreshToken?: string;
+        expiresIn?: number;
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        accessToken?: string;
+        refreshToken?: string;
+        expiresAt?: number;
+        error?: string;
     }
 }
