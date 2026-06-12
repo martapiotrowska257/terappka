d83269f5a2a749deb8fe71b239e866d31ab90540
szywat <szywat04@gmail.com>
2026-06-12 22:15:54 +0200

    Extended Toast types

--- frontend/terappka-app/src/types/toast.ts
+++ frontend/terappka-app/src/types/toast.ts
@@ -1,1 +1,5 @@
-export type ToastType = "success" | "error" | "warning" | "info";
+export type ToastMessageType = "success" | "error" | "warning" | "info";
+export type ToastType = {
+  message: string;
+  type: ToastMessageType;
+};
