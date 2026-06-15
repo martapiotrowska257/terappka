f38199fe84c6e50baa7eadd5fed340b2fcda8eaf
szywat <szywat04@gmail.com>
2026-06-15 05:25:17 +0200

    Allowed methods for CORS

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -12,7 +12,14 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000"]}})
+    CORS(app, resources={
+        r"/*": {
+            "origins": ["http://localhost:3000", "http://127.0.0.1:3000"],
+            "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"],
+            "supports_credentials": True
+        }
+    })
     db.init_app(app)
     
     socketio.init_app(app)
